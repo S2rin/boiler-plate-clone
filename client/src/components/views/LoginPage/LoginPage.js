b4b221df5194cf457d118d0c3de589419a60fbc0
surin kim <ksr95703@gmail.com>
2020-09-17 21:56:04 +0900

    로그인 페이지 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); // 이벤트 
+
+        console.log('Email', Email)
+        console.log('Password', Password)
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection:'column'}}
+                onSubmit={onSubmitHandler}>
+                <lable>Email</lable>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <lable>Password</lable>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                
+                <br />
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
